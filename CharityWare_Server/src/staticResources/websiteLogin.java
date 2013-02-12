4f4dcb9c0a54a5a7e9cc209a20d31e3c05799dbb
Alex Martin <a.martin.12@ucl.ac.uk>
2013-02-12 11:25:06 +0000

    Added web login page.

---
+++ CharityWare_Server/src/staticResources/websiteLogin.java
@@ -0,0 +1,16 @@
+package staticResources;
+
+public class websiteLogin {
+	
+	public boolean login(String userName, String password, String salt)
+	{
+		return true;
+	}
+	
+	public boolean isAuthenticated(String userName, String URL)
+	{
+		return true;
+	}
+	
+
+}
