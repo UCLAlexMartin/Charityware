9d7d12d70b5441dfc3424419959e4ea239e03096
Vasileios Angelopoulos <v.angelopoulos.12@ucl.ac.uk>
2013-02-15 18:48:46 +0000

    FilledFormService corrected.

--- CharityWare_Server/src/RESTCharityServices/FilledFormService.java
+++ CharityWare_Server/src/RESTCharityServices/FilledFormService.java
@@ -1,4 +1,6 @@
 package RESTCharityServices;
+import java.lang.reflect.Type;
+import java.net.URLDecoder;
 import java.util.List;
 
 import charityHibernateEntities.FilledForm;
@@ -13,10 +15,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
-import org.codehaus.jackson.map.ObjectMapper;
 
-//import org.codehaus.jackson.map.type;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 @Path("/filledFormService")
 public class FilledFormService {
@@ -31,25 +33,21 @@ public class FilledFormService {
     }
 	
 	@Path("/{DBConfigPath}/filledforms/insertFilledForms")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces("text/plain")
-	public String addFilledForms(@PathParam("DBConfigPath")String DBConfigPath,String data)  {		 		 
-		try{		
-			List<FilledForm> filledforms = (List<FilledForm>)new ObjectMapper().readValue(data, List.class);
+	 @POST
+	 @Consumes(MediaType.APPLICATION_JSON)
+	 @Produces("text/plain")
+	 public String addFilledForms(@PathParam("DBConfigPath")String DBConfigPath,String data)  {      
+		try{  
+			data =data.split("=")[1];
+			data=URLDecoder.decode(data);
+			Gson gson = new GsonBuilder().setDateFormat("yyyy-dd-MM").create();
+			Type listType = new TypeToken<List<FilledForm>>() {}.getType();
+			List<FilledForm> filledForms = gson.fromJson(data, listType);
 			FilledFormManager filledFormManager = new FilledFormManager(DBConfigPath);
-			return filledFormManager.addFilledForms(filledforms).toString();
-		}
-		catch(Exception e){
-			return "0";
-			
-		}		 
-	 }
-	/*
-	@POST
-	@Path("/{DBConfigPath}/filledforms/insertFilledForms")
-	@Consumes("application/json")
-	public Response insertFilledForms(JAXBElement<List<FilledForm>> todo){
-		
-	}*/
+			return filledFormManager.addFilledForms(filledForms).toString();
+	    }
+	    catch(Exception e){
+	    	return "0";
+	    }   
+	}
 }
